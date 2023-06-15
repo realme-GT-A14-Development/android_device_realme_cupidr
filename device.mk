7c3c920e0f5f49fd02c1833d6edb544ad779d261
Omkar Chandorkar <gotenksIN@aospa.co>
2023-06-15 14:26:05 +0900

    cupidr: overlay: Setup SecureNFC for cupidr
    Also set SKU prop as NFCService checks it, bootloader doesnt seem to set it even on stock
    
    Change-Id: Ie4a08a7e93cab9bd0e6761ebc4aca0d20ff2b62e

--- device.mk
+++ device.mk
@@ -49,6 +49,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    CupidrNfcOverlay \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
